cd7d69ba6ae0dca6c5ab5e4462b5b71ad383df45
amirdks <amirhossein6168@gmail.com>
2023-01-13 00:09:56 +0330

    test

--- jahanbakhshiEshop/settings.py
+++ jahanbakhshiEshop/settings.py
@@ -136,7 +136,7 @@ MEDIA_URL = '/medias/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+# salam
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account_module.User'
 LOGIN_URL = '/accounts/login'
